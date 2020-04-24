dbee39f88cac2927a467afa1ce090147a1eed6fc
Christian Oliveira <christian.soliveira@outlook.com>
2020-04-23 23:19:39 -0300

    :bug: (typeorm) Create a transformer to convert string to number
    It was a bug founded that set a string type for numbers who not are integers

--- src/models/Transaction.ts
+++ src/models/Transaction.ts
@@ -1,17 +1,47 @@
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+  CreateDateColumn,
+  JoinColumn,
+  ManyToOne,
+} from 'typeorm';
+
+import Category from './Category';
+
+import ColumnNumericTransformer from '../transformers/ColumnNumerics';
+
+@Entity('transactions')
 class Transaction {
+  @PrimaryGeneratedColumn('uuid')
   id: string;
 
+  @Column()
   title: string;
 
-  type: 'income' | 'outcome';
+  @Column()
+  type: string;
 
+  @Column('numeric', {
+    precision: 10,
+    scale: 2,
+    transformer: new ColumnNumericTransformer(),
+  })
   value: number;
 
+  @Column()
   category_id: string;
 
-  created_at: Date;
+  @ManyToOne(() => Category)
+  @JoinColumn({ name: 'category_id' })
+  category: Category;
 
+  @UpdateDateColumn()
   updated_at: Date;
+
+  @CreateDateColumn()
+  created_at: Date;
 }
 
 export default Transaction;
